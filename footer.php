8ac90e05a03e4b74948215554fa38a17b1b95018
Johan Ehrenfors <luhcforgh@gmail.com>
2019-10-24 21:09:15 +0200

    footer v1

--- footer.php
+++ footer.php
@@ -1,3 +1,25 @@
 <?php
-  $footer = '<h2>This is a minimal footer</h2>';
+  $footer = '
+    <footer>
+      <div class="footer_wrapper">
+        <div id="footer_contact">
+          <h2>Contact us</h2>
+          <p>Plonka c/o LunchCube AB<br>Kungshamra 16A Lgh 1102<br>170 70 Solna</p>
+          <p>Email: <a href="mailto:johanehrenfors@hotmail.com">hej@plonka.se</a></p>
+          <p>Tel: <a href="tel:0046737600282">+46737600282</a></p>
+
+        </div>
+
+        <div id="footer_center"></div>
+
+        <div id="footer_additional">
+          <h2>More (placeholder) links</h2>
+          <p>About us</p>
+          <p>Career</p>
+          <p>Legal</p>
+          <p>FAQ</p>
+        </div>
+      </div>
+    </footer>
+  ';
 ?>
