ccffa2e3910910b26e56f1b93881eeb6c33eedcc
Johan Ehrenfors <luhcforgh@gmail.com>
2019-10-30 21:18:51 +0100

    Landing page, fonts, etc

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,13 +1,14 @@
 var isOpen = false;
 
+/* Short function to toggle the mobile navigation menu by adjusting
+ the (animated) height between 0 and up to 80vh height. */
 function toggleDropdownMenu() {
-
   var x = document.getElementById("mobile_menu");
   if (isOpen){
     x.style.maxHeight = '0';
   }
   else {
-    x.style.maxHeight = '80vh'; 
+    x.style.maxHeight = '80vh';
   }
   isOpen = !isOpen;
 }
